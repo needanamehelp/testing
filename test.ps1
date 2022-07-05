3e37b58ca2c584d360dd89365e34e01e13dff1a0
Kanishk Godha <kanishk.godha@live.com>
2022-07-05 11:36:34 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,3 @@
-$variable = Get-AzRoleDefinition -name "humalect" | grep Id |  %{ $_.Split(' ')[-1]; }
+$variable = az role definition list --name humalect | jq -r ".[0].name"
 $test = az ad sp create --id 5b6d1e77-3e8c-4197-a201-c01b345627f2  | jq -r '.id'
 az role assignment create -g test_group --assignee-object-id $test --role $variable
