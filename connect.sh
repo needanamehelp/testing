c0acacfa2e30d5d41c4c8cc82d9d83d5787e5fc5
Kanishk Godha <kanishk.godha@live.com>
2022-07-06 13:55:01 +0530

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,6 +1,6 @@
 echo "test"
 variable="$(az role definition list --name humalect | jq -r '.[0].name')"
-#tets="$(az ad sp create --id 5b6d1e77-3e8c-4197-a201-c01b345627f2  | jq -r '.id')"
+tets="$(az ad sp create --id 5b6d1e77-3e8c-4197-a201-c01b345627f2  | jq -r '.id')"
 echo $variable
-#echo $tets
-echo "finish"
+echo $tets
+echo "change"
