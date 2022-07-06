8dc63f10a2d5da7baa2bdfb14f8c229d0db152a5
Kanishk Godha <kanishk.godha@live.com>
2022-07-06 14:14:15 +0530

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,5 +1,5 @@
 echo "test"
 variable="$(az role definition list --name humalect | jq -r '.[0].name')"
-az ad sp create --id 5b6d1e77-3e8c-4197-a201-c01b345627f2
+test="$(az ad sp create --id 5b6d1e77-3e8c-4197-a201-c01b345627f2)"
 echo $variable
 echo "change"
